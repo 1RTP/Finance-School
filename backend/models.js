90a8c71d7a94afd4915af9d3f127ddd4ee829698
Roman Yascheritsyn <romaasericyn@gmail.com>
2026-03-27 11:45:41 +0200

    changed userSchema

--- backend/models.js
+++ backend/models.js
@@ -18,7 +18,6 @@ const participantSchema = new mongoose.Schema({
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["User", "Organizer", "Admin"], default: "User" }
 });
